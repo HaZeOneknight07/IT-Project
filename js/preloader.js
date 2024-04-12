632935619005f2861be5128fd69558986b38457a
HaZeOneknight07 <159442717+HaZeOneknight07@users.noreply.github.com>
2024-04-12 10:50:16 +0100

    .

--- js/preloader.js
+++ js/preloader.js
@@ -1,6 +1,10 @@
-window.addEventListener("load", function () {
-    // Hide the preloader
-    document.querySelector(".preloader-container").classList.add("hide");
-    // Show the main content
-    document.getElementById("main-content").classList.remove("hide");
+// preloader.js
+window.addEventListener('load', function () {
+  // Hide the preloader after the video has finished loading
+  var preloader = document.querySelector('.preloader-container');
+  var video = document.getElementById('preloader-video');
+
+  video.addEventListener('loadeddata', function () {
+      preloader.classList.add('hide');
   });
+});
