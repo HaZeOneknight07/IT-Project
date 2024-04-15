eaf3b6fc2d32c07f6a5db9f534fde796bcc36c5c
HaZeOneknight07 <159442717+HaZeOneknight07@users.noreply.github.com>
2024-04-15 09:00:12 +0100

    .

--- js/capslock.js
+++ js/capslock.js
@@ -1,9 +1,9 @@
 document.getElementById('password').addEventListener('keyup', function(event) {
-    var capsLockEnabled = event.getModifierState && event.getModifierState('CapsLock');
-    var warningElement = document.querySelector('.caps-lock-warning');
-    if (capsLockEnabled) {
-      warningElement.style.display = 'block';
-    } else {
-      warningElement.style.display = 'none';
-    }
-  });
+  var capsLockEnabled = event.getModifierState && event.getModifierState('CapsLock');
+  var warningElement = document.querySelector('.caps-lock-warning');
+  if (capsLockEnabled) {
+      warningElement.classList.add('show');
+  } else {
+      warningElement.classList.remove('show');
+  }
+});
