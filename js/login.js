2b8f3e9e65bb039faa6d1b6651cc0754a15229d6
HaZeOneknight07 <159442717+HaZeOneknight07@users.noreply.github.com>
2024-04-15 12:26:28 +0100

    .

--- js/login.js
+++ js/login.js
@@ -7,7 +7,7 @@ setTimeout(function () {
 }, 5000);
 
 var credentials = [
-  { username: "admin@gelder.co.uk", password: "Admin1" },
+  { username: "admin@gelder.co.uk", password: "ADMIN!Gelder18#" },
   { username: "surveys@gelder.co.uk", password: "GelderSurveyors1" },
   { username: "managers@gelder.co.uk", password: "GelderManagers1" },
   // Add more username/password pairs as needed using same format
