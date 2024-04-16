66df5d5e0be6c36b934ea377b2a61c0bd8ffcd03
HaZeOneknight07 <159442717+HaZeOneknight07@users.noreply.github.com>
2024-04-16 13:56:08 +0100

    .

--- js/admin.js
+++ js/admin.js
@@ -1,7 +1,6 @@
 let sidebar = document.querySelector(".sidebar");
 let closeBtn = document.querySelector("#btn");
 let searchBtn = document.querySelector(".bx-search");
-let pageNavigationBtn = document.querySelector("#pageNavigationBtn"); // Add this line
 
 closeBtn.addEventListener("click", () => {
   sidebar.classList.toggle("open");
@@ -9,22 +8,17 @@ closeBtn.addEventListener("click", () => {
 });
 
 searchBtn.addEventListener("click", () => {
-  // Sidebar open when you click on the search icon
+  // Sidebar open when you click on the search iocn
   sidebar.classList.toggle("open");
   menuBtnChange(); //calling the function(optional)
-});
-
-// Add event listener for the page navigation button
-pageNavigationBtn.addEventListener("click", () => {
-  togglePageNavigation(); // Call the function to toggle page navigation container
 });
 
 // following are the code to change sidebar button(optional)
 function menuBtnChange() {
   if (sidebar.classList.contains("open")) {
-    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the icons class
+    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the iocns class
   } else {
-    closeBtn.classList.replace("bx-menu-alt-right", "bx-menu"); //replacing the icons class
+    closeBtn.classList.replace("bx-menu-alt-right", "bx-menu"); //replacing the iocns class
   }
 }
 
