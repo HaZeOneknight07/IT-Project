1bd1ec50a47c4e9615afe84e386f60dece14a2b0
HaZeOneknight07 <159442717+HaZeOneknight07@users.noreply.github.com>
2024-04-18 13:17:26 +0100

    .

--- js/login.js
+++ js/login.js
@@ -1,68 +1,61 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // Delay the hiding of preloader and showing login content after 5 seconds
-  setTimeout(function () {
-    document.querySelector(".preloader-container").classList.add("hide");
-    document.getElementById("main-content").classList.remove("hide");
-  }, 5000);
+// Login Validation Script
 
-  var credentials = [
-    {
-      username: "admin@gelder.co.uk",
-      password: "ADMIN!Gelder18#",
-      name: "Admin",
-      job: "Administrator",
-    },
-    {
-      username: "surveys@gelder.co.uk",
-      password: "GelderSurveyors1",
-      name: "Surveyor",
-      job: "Surveyor",
-    },
-    {
-      username: "managers@gelder.co.uk",
-      password: "GelderManagers1",
-      name: "Manager",
-      job: "Contracts Manager",
-    },
-    // Add more username/password/name/job pairs as needed using same format
-  ];
+// Delay the hiding of preloader and showing login content after 5 seconds
+setTimeout(function () {
+  document.querySelector(".preloader-container").classList.add("hide");
+  document.getElementById("main-content").classList.remove("hide");
+}, 5000);
 
-  function redirectToHome() {
+var credentials = [
+  { username: "admin@gelder.co.uk", password: "ADMIN!Gelder18#" },
+  { username: "surveys@gelder.co.uk", password: "GelderSurveyors1" },
+  { username: "managers@gelder.co.uk", password: "GelderManagers1" },
+  // Add more username/password pairs as needed using same format
+];
+
+function redirectToHome() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    var user = credentials.find(cred => cred.username === username);
-
-    if (user && password === user.password) {
-      document.getElementById("name").value = user.name;
-      document.getElementById("job").value = user.job;
-
-      localStorage.setItem("loggedInUsername", username);
-      localStorage.setItem("loggedInPassword", password);
-
-      if (username === "managers@gelder.co.uk") {
-        window.location.href = "scheduleoption.html";
-      } else if (username === "surveyors@gelder.co.uk") {
-        window.location.href = "hub.html";
-      } else if (username === "admin@gelder.co.uk") {
-        window.location.href = "admin.html";
+  
+    for (var i = 0; i < credentials.length; i++) {
+      if (
+        username === credentials[i].username &&
+        password === credentials[i].password
+      ) {
+        // Store data in local storage
+        localStorage.setItem("loggedInUsername", username);
+        localStorage.setItem("loggedInPassword", password);
+  
+        // Redirect to Managers Version
+        if (username === "managers@gelder.co.uk") {
+          window.location.href = "scheduleoption.html"; // Redirect to Schedule Option Page
+        }
+        // Redirect to Surveyors Version
+        else if (username === "surveyors@gelder.co.uk") {
+          window.location.href = "hub.html"; // Redirect to Hub Page
+        }
+        // Redirect to Admin Panel
+        else if (username === "admin@gelder.co.uk") {
+          window.location.href = "admin.html"; // Redirect to Admin Panel
+        }
+        return;
       }
-    } else {
-      alert("Invalid login credentials. Please try again.");
     }
+    
+    // If no match found in the loop
+    alert("Invalid login credentials. Please try again.");
   }
+  
 
-  document.getElementById("loginForm").addEventListener("submit", function (event) {
-    event.preventDefault();
-    redirectToHome();
-  });
+// Caps Lock Indicator Script
 
-  document.getElementById("password").addEventListener("keyup", function (event) {
-    var capsLockEnabled = event.getModifierState && event.getModifierState("CapsLock");
-    var warningElement = document.querySelector(".caps-lock-warning");
-    if (capsLockEnabled) {
-      warningElement.style.display = "block";
-    } else {
-      warningElement.style.display = "none";
-    }
-  });
+document.getElementById("password").addEventListener("keyup", function (event) {
+  var capsLockEnabled =
+    event.getModifierState && event.getModifierState("CapsLock");
+  var warningElement = document.querySelector(".caps-lock-warning");
+  if (capsLockEnabled) {
+    warningElement.style.display = "block";
+  } else {
+    warningElement.style.display = "none";
+  }
 });
