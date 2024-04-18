27bd1040697402c908c142c46063e10db4d5ec41
HaZeOneknight07 <159442717+HaZeOneknight07@users.noreply.github.com>
2024-04-18 13:30:42 +0100

    .

--- js/admin.js
+++ js/admin.js
@@ -72,6 +72,31 @@ function enableClickableElements() {
   });
 }
 
+// Sidebar functionality
+var sidebar = document.querySelector(".sidebar");
+var closeBtn = document.querySelector("#btn");
+var searchBtn = document.querySelector(".bx-search");
+
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("open");
+  menuBtnChange(); //calling the function(optional)
+});
+
+searchBtn.addEventListener("click", () => {
+  // Sidebar open when you click on the search iocn
+  sidebar.classList.toggle("open");
+  menuBtnChange(); //calling the function(optional)
+});
+
+// following are the code to change sidebar button(optional)
+function menuBtnChange() {
+  if (sidebar.classList.contains("open")) {
+    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the iocns class
+  } else {
+    closeBtn.classList.replace("bx-menu-alt-right", "bx-menu"); //replacing the iocns class
+  }
+}
+
 // Function to handle GitHub link
 function handleGitHubLink() {
   var currentUser = document
