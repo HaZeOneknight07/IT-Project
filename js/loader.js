f1d3d01508efe212659ec0ff6837b272a73df63d
HaZeOneknight07 <159442717+HaZeOneknight07@users.noreply.github.com>
2024-04-29 15:24:38 +0100

    .

--- js/loader.js
+++ js/loader.js
@@ -2,14 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
     // Show the preloader
     document.querySelector(".preloader-container").style.display = "block";
 
-    // Hide the main content
+    // Hide the main content (if needed)
     document.getElementById("main-content").style.display = "none";
 });
 
 window.addEventListener("load", function () {
-    // Hide the preloader
-    document.querySelector(".preloader-container").style.display = "none";
-
-    // Show the main content
-    document.getElementById("main-content").style.display = "block";
+    // Hide the preloader after a delay
+    setTimeout(function () {
+        document.querySelector(".preloader-container").style.display = "none";
+        // Show the main content (if needed)
+        document.getElementById("main-content").style.display = "block";
+    }, 1000); // Adjust the delay (in milliseconds) as needed
 });
