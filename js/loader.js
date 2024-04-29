214261b3d6d5107c9dfcb28fa771e57f35adf155
HaZeOneknight07 <159442717+HaZeOneknight07@users.noreply.github.com>
2024-04-29 15:13:17 +0100

    .

---
+++ js/loader.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function () {
+    // Hide the preloader
+    document.querySelector(".preloader-container").style.display = "none";
+
+    // Show the main content
+    document.getElementById("main-content").style.display = "block";
+});
