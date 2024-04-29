b3f3e3c92d9db485b0890e190a85b93c41588937
HaZeOneknight07 <159442717+HaZeOneknight07@users.noreply.github.com>
2024-04-29 15:20:49 +0100

    .

--- js/loader.js
+++ js/loader.js
@@ -1,4 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Show the preloader
+    document.querySelector(".preloader-container").style.display = "block";
+
+    // Hide the main content
+    document.getElementById("main-content").style.display = "none";
+});
+
 window.addEventListener("load", function () {
     // Hide the preloader
-    document.querySelector("preloader-container").style.display = "none";
+    document.querySelector(".preloader-container").style.display = "none";
+
+    // Show the main content
+    document.getElementById("main-content").style.display = "block";
 });
