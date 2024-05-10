c3da6305f7e3e5fafca9cbebd51a005b95110747
HaZeOneknight07 <159442717+HaZeOneknight07@users.noreply.github.com>
2024-05-10 08:00:41 +0100

    .

--- js/login.js
+++ js/login.js
@@ -92,3 +92,32 @@ document.getElementById("password").addEventListener("keyup", function (event) {
     warningElement.style.display = "none";
   }
 });
+
+// Auto Fill Check for Active Class
+
+function handleAutofill(field) {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  if (field.value !== "") {
+    field.nextElementSibling.classList.add("active");
+  } else {
+    field.nextElementSibling.classList.remove("active");
+  }
+
+  // Check if both username and password fields are filled
+  if (username !== "" && password !== "") {
+    // Loop through credentials array to check for match
+    for (var i = 0; i < credentials.length; i++) {
+      if (
+        username === credentials[i].username &&
+        password === credentials[i].password
+      ) {
+        // Add "active" class to labels
+        document.querySelector("label[for='username']").classList.add("active");
+        document.querySelector("label[for='password']").classList.add("active");
+        return;
+      }
+    }
+  }
+}
