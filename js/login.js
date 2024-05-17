730bc96fb5de9c970121f01e09154851faa2b041
HaZeOneknight07 <159442717+HaZeOneknight07@users.noreply.github.com>
2024-05-17 09:22:57 +0100

    .

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ setTimeout(function () {
   document.getElementById("main-content").classList.remove("hide");
 }, 5000);
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const firebaseConfig = {
     apiKey: "AIzaSyCq4mZfp2XSASkrdoCt4BrQHKsMpMdPz20",
     authDomain: "rates-website-db.firebaseapp.com",
@@ -12,95 +12,99 @@ document.addEventListener("DOMContentLoaded", function() {
     projectId: "rates-website-db",
     storageBucket: "rates-website-db.appspot.com",
     messagingSenderId: "841721059290",
-    appId: "1:841721059290:web:19edd2668bb7b86ca112e3"
+    appId: "1:841721059290:web:19edd2668bb7b86ca112e3",
   };
-  
+
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   const auth = firebase.auth();
   const database = firebase.database();
-  
+
   // Add event listener for form submission
   const loginForm = document.getElementById("main-content");
   if (loginForm) {
-      loginForm.addEventListener("submit", function(event) {
-          event.preventDefault();
-          redirectToHome();
-      });
+    loginForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+      redirectToHome();
+    });
   } else {
-      console.error("Login form element not found.");
+    console.error("Login form element not found.");
   }
-  
+
   // Listen for input events on username and password fields
   const usernameField = document.getElementById("username");
   const passwordField = document.getElementById("password");
   if (usernameField && passwordField) {
-      usernameField.addEventListener("input", function() {
-          handleAutofill(this);
-      });
-  
-      passwordField.addEventListener("input", function() {
-          handleAutofill(this);
-      });
+    usernameField.addEventListener("input", function () {
+      handleAutofill(this);
+    });
+
+    passwordField.addEventListener("input", function () {
+      handleAutofill(this);
+    });
   } else {
-      console.error("Username or password field not found.");
+    console.error("Username or password field not found.");
   }
-  
+
   function handleAutofill(field) {
-      if (field.value !== "") {
-          field.nextElementSibling.classList.add("active");
-      } else {
-          field.nextElementSibling.classList.remove("active");
-      }
+    if (field.value !== "") {
+      field.nextElementSibling.classList.add("active");
+    } else {
+      field.nextElementSibling.classList.remove("active");
+    }
   }
-  
+
   function redirectToHome() {
-      var username = document.getElementById("username").value.toLowerCase();
-      var password = document.getElementById("password").value;
-  
-      auth.signInWithEmailAndPassword(username, password)
-          .then((userCredential) => {
-              var user = userCredential.user;
-  
-              // Store data in local storage
-              localStorage.setItem("loggedInUsername", username);
-              localStorage.setItem("loggedInUserEmail", username);
-  
-              // Fetch user role from Realtime Database using UID
-              database.ref('users/' + user.uid).once('value').then((snapshot) => {
-                  var role = snapshot.val().role;
-  
-                  // Redirect based on user role
-                  if (role === "manager") {
-                      window.location.href = "scheduleoption.html";
-                  } else if (role === "surveyor") {
-                      window.location.href = "hub.html";
-                  } else if (role === "admin") {
-                      window.location.href = "admin.html";
-                  } else {
-                      alert("Invalid user role.");
-                  }
-              });
-          })
-          .catch((error) => {
-              alert("Invalid login credentials. Please try again.");
-              console.error(error);
+    var username = document.getElementById("username").value.toLowerCase();
+    var password = document.getElementById("password").value;
+
+    auth
+      .signInWithEmailAndPassword(username, password)
+      .then((userCredential) => {
+        var user = userCredential.user;
+
+        // Store data in local storage
+        localStorage.setItem("loggedInUsername", username);
+        localStorage.setItem("loggedInUserEmail", username);
+
+        // Fetch user role from Realtime Database using UID
+        database
+          .ref("users/" + user.uid)
+          .once("value")
+          .then((snapshot) => {
+            var role = snapshot.val().role;
+
+            // Redirect based on user role
+            if (role === "manager") {
+              window.location.href = "scheduleoption.html";
+            } else if (role === "surveyor") {
+              window.location.href = "hub.html";
+            } else if (role === "admin") {
+              window.location.href = "admin.html";
+            } else {
+              alert("Invalid user role.");
+            }
           });
-  }
-  
-  // Caps Lock Indicator Script
-  const passwordField = document.getElementById("password");
-  if (passwordField) {
-      passwordField.addEventListener("keyup", function(event) {
-          var capsLockEnabled = event.getModifierState && event.getModifierState("CapsLock");
-          var warningElement = document.querySelector(".caps-lock-warning");
-          if (capsLockEnabled) {
-              warningElement.style.display = "block";
-          } else {
-              warningElement.style.display = "none";
-          }
+      })
+      .catch((error) => {
+        alert("Invalid login credentials. Please try again.");
+        console.error(error);
       });
+  }
+
+  // Caps Lock Indicator Script
+  if (passwordField) {
+    passwordField.addEventListener("keyup", function (event) {
+      var capsLockEnabled =
+        event.getModifierState && event.getModifierState("CapsLock");
+      var warningElement = document.querySelector(".caps-lock-warning");
+      if (capsLockEnabled) {
+        warningElement.style.display = "block";
+      } else {
+        warningElement.style.display = "none";
+      }
+    });
   } else {
-      console.error("Password field not found.");
+    console.error("Password field not found.");
   }
 });
